451da757fe69c13f19a6924049db99402fedf02c
Paul Downey <psd@whatfettle.com>
2017-06-06 14:05:13 +0100

    Script to convert documents to images

---
+++ bin/images.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+for path in "$@"
+do
+  id=$(echo "$path"|sed -e 's/^cache\/attachment\///' -e 's/\/.*$//')
+  dir="image/attachment/$id/"
+
+  suffix=$(echo ${path##*.} | tr '[:upper:]' '[:lower:]')
+  doc="$dir/document.$suffix"
+  pdf="$dir/document.pdf"
+
+  mkdir -p "$dir"
+
+  case "$suffix" in
+  pdf)
+    ln -f "$path" "$pdf"
+    ;;
+
+  *)
+    ln -f "$path" "$doc"
+    (
+      cd $dir
+      lowriter --headless --convert-to pdf "document.$suffix"
+    )
+    ;;
+  esac
+
+  pdftoppm -rx 300 -ry 300 -png "$pdf" "$dir/page"
+done
